3e51384a31e5fe8207a7c9c6eb0993bc55852394
unknown <praveenkumarnulu@gmail.com>
2018-09-08 17:14:51 +0530

    added testing post request

--- app.py
+++ app.py
@@ -8,33 +8,33 @@ api = Api(app)
 def hello_world():
     return 'Hello World!'
 
-# class User(Resource):
-#     def post(self):
-#         response = {
-#             "version": "1.0",
-#             "response": {
-#                 "outputSpeech": {
-#                     "type": "PlainText",
-#                     "text": "Hello all, this is a sample text for testing purpose",
-# 
-#                 },
-#                 "card": {
-#                     "type": "Simple",
-#                     "title": "Testing Purpose",
-#                     "content": "Nothing much content",
-#                 },
-#                 "reprompt": {
-#                     "outputSpeech": {
-#                         "type": "PlainText",
-#                         "text": "Can i help you with anything else.",
-# 
-#                     }
-#                 },
-#             }
-#         }
-#         return response
-# 
-# 
-# api.add_resource(User, '/user')
-# app.run()
+class User(Resource):
+    def post(self):
+        response = {
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "Hello all, this is a sample text for testing purpose",
+
+                },
+                "card": {
+                    "type": "Simple",
+                    "title": "Testing Purpose",
+                    "content": "Nothing much content",
+                },
+                "reprompt": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": "Can i help you with anything else.",
+
+                    }
+                },
+            }
+        }
+        return response
+
+
+api.add_resource(User, '/user')
+app.run()
 	
