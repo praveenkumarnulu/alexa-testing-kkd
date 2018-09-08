bcaf71c2350ae2f4e582cc25bf421a544f497148
unknown <praveenkumarnulu@gmail.com>
2018-09-08 09:10:51 +0530

    make it better

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class User(Resource):
+    def post(self):
+        response = {
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "Hello all, this is a sample text for testing purpose",
+
+                },
+                "card": {
+                    "type": "Simple",
+                    "title": "Testing Purpose",
+                    "content": "Nothing much content",
+                },
+                "reprompt": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": "Can i help you with anything else.",
+
+                    }
+                },
+            }
+        }
+        return response
+
+
+api.add_resource(User, '/user')
+app.run(debug=True)
