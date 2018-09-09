daa550b146ac0e5c1d8a345aa5788430b4deff59
unknown <praveenkumarnulu@gmail.com>
2018-09-09 12:43:04 +0530

    checking another time

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
@@ -8,35 +9,61 @@ api = Api(app)
 @app.route('/')
 def hello():
     return 'Hellow orldddddd'
-   
 
-class User(Resource):
-    def post(self):
-        response = {
-            "version": "1.0",
-            "response": {
+
+#
+# class User(Resource):
+#     def post(self):
+#         response = {
+#             "version": "1.0",
+#             "response": {
+#                 "outputSpeech": {
+#                     "type": "PlainText",
+#                     "text": "Hello all, this is a sample text for testing purpose",
+#
+#                 },
+#                 "card": {
+#                     "type": "Simple",
+#                     "title": "Testing Purpose",
+#                     "content": "Nothing much content",
+#                 },
+#                 "reprompt": {
+#                     "outputSpeech": {
+#                         "type": "PlainText",
+#                         "text": "Can i help you with anything else.",
+#
+#                     }
+#                 },
+#             }
+#         }
+#         return response
+
+@app.route('/user', methods=['POST'])
+def post_testing():
+    response = {
+        "version": "1.0",
+        "response": {
+            "outputSpeech": {
+                "type": "PlainText",
+                "text": "Hello all, this is a sample text for testing purpose",
+
+            },
+            "card": {
+                "type": "Simple",
+                "title": "Testing Purpose",
+                "content": "Nothing much content",
+            },
+            "reprompt": {
                 "outputSpeech": {
                     "type": "PlainText",
-                    "text": "Hello all, this is a sample text for testing purpose",
+                    "text": "Can i help you with anything else.",
 
-                },
-                "card": {
-                    "type": "Simple",
-                    "title": "Testing Purpose",
-                    "content": "Nothing much content",
-                },
-                "reprompt": {
-                    "outputSpeech": {
-                        "type": "PlainText",
-                        "text": "Can i help you with anything else.",
-
-                    }
-                },
-            }
+                }
+            },
         }
-        return response
+    }
+    return jsonify(response), 201
 
 
-api.add_resource(User, '/user')
+# api.add_resource(User, '/user')
 app.run()
-	
