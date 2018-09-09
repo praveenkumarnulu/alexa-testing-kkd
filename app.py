d599c8affaa4c3a8ff0cdcdc925d47f084143d21
unknown <praveenkumarnulu@gmail.com>
2018-09-09 12:28:52 +0530

    checking2

--- app.py
+++ app.py
@@ -5,33 +5,38 @@ app = Flask(__name__)
 api = Api(app)
 
 
-# class User(Resource):
-#     def post(self):
-#         response = {
-#             "version": "1.0",
-#             "response": {
-#                 "outputSpeech": {
-#                     "type": "PlainText",
-#                     "text": "Hello all, this is a sample text for testing purpose",
-# 
-#                 },
-#                 "card": {
-#                     "type": "Simple",
-#                     "title": "Testing Purpose",
-#                     "content": "Nothing much content",
-#                 },
-#                 "reprompt": {
-#                     "outputSpeech": {
-#                         "type": "PlainText",
-#                         "text": "Can i help you with anything else.",
-# 
-#                     }
-#                 },
-#             }
-#         }
-#         return response
-# 
-# 
-# api.add_resource(User, '/user')
-# app.run()
+@app.route('/')
+def hello():
+    return 'Hellow orldddddd'
+   
+
+class User(Resource):
+    def post(self):
+        response = {
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "Hello all, this is a sample text for testing purpose",
+
+                },
+                "card": {
+                    "type": "Simple",
+                    "title": "Testing Purpose",
+                    "content": "Nothing much content",
+                },
+                "reprompt": {
+                    "outputSpeech": {
+                        "type": "PlainText",
+                        "text": "Can i help you with anything else.",
+
+                    }
+                },
+            }
+        }
+        return response
+
+
+api.add_resource(User, '/user')
+app.run()
 	
