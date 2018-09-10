098fe4b90849e2344a9b0404826b29316b8455ac
Praveen Kumar <praveenkumarnulu@gmail.com>
2018-09-10 18:17:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import jsonify
 from flask_restful import Api, Resource, reqparse
 
@@ -8,7 +8,7 @@ api = Api(app)
 
 @app.route('/')
 def hello():
-    return 'Hellow orldddddd'+'Nothing mucjhhhhhh'
+    return 'Hellow orlds!!!'
 
 
 #
@@ -60,11 +60,140 @@ def post_testing():
 
                 }
             },
+			"shouldEndSession": False
         }
     }
     return jsonify(response), 201
 
-
+@app.route('/user/full', methods=['POST'])
+def post_testing_full():
+  result = {
+  "version": "1.0",
+  "sessionAttributes": {
+    "supportedHoriscopePeriods": {
+      "daily": True,
+      "weekly": False,
+      "monthly": False
+    }
+  },
+  "response": {
+    "outputSpeech": {
+      "type": "PlainText",
+      "text": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?"
+    },
+    "card": {
+      "type": "Simple",
+      "title": "Horoscope",
+      "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
+    },
+    "reprompt": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "Can I help you with anything else?"
+      }
+    },
+    "shouldEndSession": False
+   }
+  }
+  return jsonify(result)
+  
+@app.route('/user/msd', methods=['POST'])
+def msd_post():
+  intent = request.get_json(silent=True)
+  if intent["request"]["intent"]["name"] == 'GetNewFactIntent':
+    result = {
+  "version": "1.0",
+  "sessionAttributes": {
+    "supportedHoriscopePeriods": {
+      "daily": True,
+      "weekly": False,
+      "monthly": False
+    }
+  },
+  "response": {
+    "outputSpeech": {
+      "type": "PlainText",
+      "text": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless. Can I help you with anything else?"
+    },
+    "card": {
+      "type": "Simple",
+      "title": "Horoscope",
+      "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
+    },
+    "reprompt": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "Can I help you with anything else?"
+      }
+    },
+    "shouldEndSession": False
+   }
+  }
+    return jsonify(result)
+  
+  elif intent["request"]["intent"]["name"] == 'CricketIntent':
+    result = {
+  "version": "1.0",
+  "sessionAttributes": {
+    "supportedHoriscopePeriods": {
+      "daily": True,
+      "weekly": False,
+      "monthly": False
+    }
+  },
+  "response": {
+    "outputSpeech": {
+      "type": "PlainText",
+      "text": "MSD is a great cricketer"
+    },
+    "card": {
+      "type": "Simple",
+      "title": "MSD",
+      "content": "MSD is a great cricketer"
+    },
+    "reprompt": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "Can I help you with anything else?"
+      }
+    },
+    "shouldEndSession": False
+   }
+  }
+    return jsonify(result)
+  else:
+    result = {
+  "version": "1.0",
+  "sessionAttributes": {
+    "supportedHoriscopePeriods": {
+      "daily": True,
+      "weekly": False,
+      "monthly": False
+    }
+  },
+  "response": {
+    "outputSpeech": {
+      "type": "PlainText",
+      "text": "May be I need to update my dictionary to answer you. Your're brilliant than me."
+    },
+    "card": {
+      "type": "Simple",
+      "title": "Oh!!",
+      "content": "May be I need to update my dictionary to answer you. Your're brilliant than me."
+    },
+    "reprompt": {
+      "outputSpeech": {
+        "type": "PlainText",
+        "text": "Can I help you with anything else?"
+      }
+    },
+    "shouldEndSession": False
+   }
+  }
+    return jsonify(result)  
+    
+	
+	
 # api.add_resource(User, '/user')
 if __name__=='__main__':
     app.run(debug=True)
